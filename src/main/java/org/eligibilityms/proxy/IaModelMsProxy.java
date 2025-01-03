5c87f35887b7f2803bb124a0f8edf16c0dbfb5a7
Soukaina Jabri <soukaina.jabri333@gmail.com>
2025-01-03 22:01:10 +0100

    change model ms feign client 2

--- src/main/java/org/eligibilityms/proxy/IaModelMsProxy.java
+++ src/main/java/org/eligibilityms/proxy/IaModelMsProxy.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
  *  Feign for communication with IaModel-ms service
  *  Ribbon for loadBalancing
  */
-@FeignClient(name = "IAMODEL-MS")
-@RibbonClient(name = "IAMODEL-MS")
+@FeignClient(name = "iamodel-ms")
+@RibbonClient(name = "iamodel-ms")
 public interface IaModelMsProxy {
 
     /***
